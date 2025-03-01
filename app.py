5046dfbc71e871dfda3ad10cce822756810d2124
Zaryab Irfan <164354254+zaryabjiya@users.noreply.github.com>
2025-03-01 14:53:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 from io import BytesIO
 
-st.set_page_config(page_title="Data Sweeper", layout="white")
+st.set_page_config(page_title="Data Sweeper", layout="wide")
 
 #custom css
 st.markdown(
